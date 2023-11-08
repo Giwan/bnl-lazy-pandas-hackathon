ca251578eb10f1de60194c1bbb13be43726481d5
Bogdan Deren <None>
2023-11-08 14:49:09 +0100

    page 3

---
+++ app/step3/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from '../page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Here are three recommendations on how to furnish the room...</h1>
+    </main>
+  )
+}

9937e82c9fb4667c8ca996c35973fec99bd1a0bd
bderen <deren.bogdan@gmail.com>
2023-11-08 16:03:05 +0100

    grid

--- app/step2/page.js
+++ app/step2/page.js
@@ -4,7 +4,20 @@ import styles from '../page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Here are some suggestions from Adevinta to furnish your new apartment with...</h1>
+      <div className={styles.header}>
+        Here are <strong>three</strong> recommendations on how to furnish the room
+      </div>
+      <div className={styles.grid}>
+        <div className={styles.card}>
+          1
+        </div>
+        <div className={styles.card}>
+          2
+        </div>
+        <div className={styles.card}>
+          3
+        </div>
+      </div>
     </main>
   )
 }

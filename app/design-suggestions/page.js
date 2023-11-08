56e2afc3f1b39078611a4a2176f3ea02ad76dea3
bderen <deren.bogdan@gmail.com>
2023-11-08 17:18:51 +0100

    links

--- app/design-suggestions/page.js
+++ app/design-suggestions/page.js
@@ -1,5 +1,6 @@
-import Image from 'next/image'
+import Link from 'next/link';
 import styles from '../page.module.css'
+import ChatInput from '@/components/ChatInput/ChatInput';
 
 export default function Home() {
   return (
@@ -9,13 +10,19 @@ export default function Home() {
       </div>
       <div className={styles.grid}>
         <div className={styles.card}>
+          <Link href='/step3'>
           <img src='https://res.cloudinary.com/drerw82nm/image/upload/c_thumb,g_auto,h_200,w_800/lazypandas/c5uwjt8qzidq2ommpczp.png' />
+          </Link>
         </div>
         <div className={styles.card}>
+        <Link href='/step3'>
           <img src='https://res.cloudinary.com/drerw82nm/image/upload/c_thumb,g_auto,h_200,w_800/lazypandas/qwxccqxbr4bvoxefq3ek.jpg' />
+          </Link>
         </div>
         <div className={styles.card}>
+        <Link href='/step3'>
           <img src='https://res.cloudinary.com/drerw82nm/image/upload/c_thumb,g_auto,h_200,w_800/lazypandas/bfajkvtle5bddkd912hl.png' />
+          </Link>
         </div>
       </div>
       <div className={styles.chat}>
@@ -30,6 +37,7 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <ChatInput />
     </main>
   )
 }

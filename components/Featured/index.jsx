04191a2b1dd341ba5d9fbb850f914958789dc3f1
bderen <deren.bogdan@gmail.com>
2023-11-09 10:51:04 +0100

    rooms

--- components/Featured/index.jsx
+++ components/Featured/index.jsx
@@ -2,23 +2,24 @@
 import Slider from "react-slick";
 import React, { Component } from "react";
 import Image from "next/image";
+import Link from 'next/link';
 import ChatInput from '@/components/ChatInput/ChatInput';
 // CAROUSEL DATA
 
 const postData = [
-    {
-        imgSrc: '/images/featured/feat1.jpg',
-    },
-    {
-        imgSrc: '/images/featured/feat2.jpg',
-    },
-    {
-        imgSrc: '/images/featured/feat1.jpg',
-    },
-    {
-        imgSrc: '/images/featured/feat2.jpg',
-    }
-]
+        {
+          name: "Room #1",
+          img:"https://res.cloudinary.com/drerw82nm/image/upload/c_fill,h_630,w_630/lazypandas/dlv1pufje7mmycc7cprw.jpg",
+        },
+        {
+          name: "Room #2",
+          img:"https://res.cloudinary.com/drerw82nm/image/upload/c_fill,h_630,w_630/lazypandas/f3c70yfa1gesy4ujgt1p.jpg",
+        },
+        {
+          name: "Room #3",
+          img:"https://res.cloudinary.com/drerw82nm/image/upload/c_fill,g_center,h_630,w_630/lazypandas/dkzhq5sqnsinu8fy0xwb.jpg",
+        }
+      ]
 
 // CAROUSEL SETTINGS
 
@@ -76,7 +77,7 @@ export default class MultipleItems extends Component {
 
         return (
             <div className="bg-bgblue marginFeature bg-featured">
-                <div className='mx-auto max-w-7xl sm:py-4 lg:px-8 '>
+                <div className='mx-auto max-w-7xl lg:px-8 '>
 
                     
                     <Slider {...settings}>
@@ -84,7 +85,9 @@ export default class MultipleItems extends Component {
                             <div key={i}>
 
                                 <div className='bg-transparent m-3 pb-12 my-10 rounded-3xl'>
-                                    <Image src={items.imgSrc} alt="gaby" width={636} height={620} className="rounded-2xl" />
+                                    <Link href="/items-list">
+                                    <Image src={items.img} alt="gaby" width={636} height={620} className="rounded-2xl" />
+                                    </Link> 
                                 </div>
 
                             </div>

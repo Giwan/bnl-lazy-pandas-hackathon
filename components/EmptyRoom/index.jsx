e4820b92a6cd24ae3f1c9ac817207437f426c4c0
bderen <deren.bogdan@gmail.com>
2023-11-09 11:16:59 +0100

    ...

--- components/EmptyRoom/index.jsx
+++ components/EmptyRoom/index.jsx
@@ -1,12 +1,19 @@
+'use client';
+import { useState } from 'react';
 import Image from "next/image";
 import ChatInput from '@/components/ChatInput/ChatInput';
 import styles from './emptyRoom.module.css';
 const index = () => {
+    const [hidden, setHidden] = useState(true);
+    const handleClick = () => {
+        if (hidden) setHidden(!hidden);
+        if (!hidden) location.href = '/step3';
+    }
     return (
         <div id='empty-room' className='mx-auto max-w-7xl sm:py-4 lg:px-8'>
             <div className='grid grid-cols-1 my-16'>
                 <Image className={styles.image} src="/images/empty_room5.webp" alt="room-image" height={333} width={600} />
-                <div className={styles.chat}>
+                <div className={`${styles.chat} ${hidden && styles.hidden}`}>
                     <div className={styles.chat_question}>
                     <div className={styles.message_text}>
                         Can you make it more green?
@@ -18,7 +25,7 @@ const index = () => {
                     </div>
                     </div>
                 </div>
-                <ChatInput className={styles.chatInput}  onClick={() => location.href = '/step3'}/>
+                <ChatInput className={styles.chatInput}  onClick={handleClick}/>
             </div>
         </div>
     )
